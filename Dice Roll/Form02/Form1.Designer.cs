e5623ebc74060195211c6489bcd6979aabdf9b27
Emo-77 <emirip11@gmail.com>
2025-01-31 23:37:11 +0300

    Dice Roll

---
+++ Dice Roll/Form02/Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace Form02
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Zar1 = new System.Windows.Forms.TextBox();
+            this.Zar2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.a = new System.Windows.Forms.Label();
+            this.p1 = new System.Windows.Forms.PictureBox();
+            this.p2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Zar1
+            // 
+            this.Zar1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Zar1.Location = new System.Drawing.Point(82, 86);
+            this.Zar1.Multiline = true;
+            this.Zar1.Name = "Zar1";
+            this.Zar1.Size = new System.Drawing.Size(124, 50);
+            this.Zar1.TabIndex = 0;
+            // 
+            // Zar2
+            // 
+            this.Zar2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Zar2.Location = new System.Drawing.Point(350, 86);
+            this.Zar2.Multiline = true;
+            this.Zar2.Name = "Zar2";
+            this.Zar2.Size = new System.Drawing.Size(124, 50);
+            this.Zar2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Location = new System.Drawing.Point(172, 477);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(190, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "AT";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // a
+            // 
+            this.a.AutoSize = true;
+            this.a.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.a.Location = new System.Drawing.Point(172, 166);
+            this.a.Name = "a";
+            this.a.Size = new System.Drawing.Size(190, 69);
+            this.a.TabIndex = 3;
+            this.a.Text = "label1";
+            // 
+            // p1
+            // 
+            this.p1.Location = new System.Drawing.Point(112, 289);
+            this.p1.Name = "p1";
+            this.p1.Size = new System.Drawing.Size(120, 150);
+            this.p1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p1.TabIndex = 4;
+            this.p1.TabStop = false;
+            // 
+            // p2
+            // 
+            this.p2.Location = new System.Drawing.Point(319, 289);
+            this.p2.Name = "p2";
+            this.p2.Size = new System.Drawing.Size(120, 150);
+            this.p2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.p2.TabIndex = 5;
+            this.p2.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(575, 567);
+            this.Controls.Add(this.p2);
+            this.Controls.Add(this.p1);
+            this.Controls.Add(this.a);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Zar2);
+            this.Controls.Add(this.Zar1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.p1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.p2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox Zar1;
+        private System.Windows.Forms.TextBox Zar2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label a;
+        private System.Windows.Forms.PictureBox p1;
+        private System.Windows.Forms.PictureBox p2;
+    }
+}
+
---
+++ Dice Roll/Form02/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Form02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Zar1.Text="";
+            Zar2.Text="";
+            a.Text="";
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            a.Text="";
+            Random z = new Random();
+            int z1=z.Next(1, 7);
+            Zar1.Text=z1.ToString();
+
+            int z2=z.Next(1, 7);
+            Zar2.Text=z2.ToString();
+
+            if(z1==z2)
+            {
+                switch (z1)
+                {
+                    case 1: a.Text="Hep Yek"; break;
+                    case 2: a.Text="Dü bara"; break;
+                    case 3: a.Text="Düş se"; break;
+                    case 4: a.Text="Dört Cehar"; break;
+                    case 5: a.Text="Dübeş"; break;
+                    case 6: a.Text="Düşeş"; break;
+
+                }
+            }
+
+
+            switch (z1)
+                {
+                    case 1: p1.Image=Image.FromFile("1.png"); break;
+                    case 2: p1.Image=Image.FromFile("2.png"); break;
+                    case 3: p1.Image=Image.FromFile("3.png"); break;
+                    case 4: p1.Image=Image.FromFile("4.png"); break;
+                    case 5: p1.Image=Image.FromFile("5.png"); break;
+                    case 6: p1.Image=Image.FromFile("6.png"); break;
+
+
+                }
+
+            switch (z2)
+            {
+                case 1: p2.Image=Image.FromFile("1.png"); break;
+                case 2: p2.Image=Image.FromFile("2.png"); break;
+                case 3: p2.Image=Image.FromFile("3.png"); break;
+                case 4: p2.Image=Image.FromFile("4.png"); break;
+                case 5: p2.Image=Image.FromFile("5.png"); break;
+                case 6: p2.Image=Image.FromFile("6.png"); break;
+
+
+            }           
+
+        }
+    }
+}
